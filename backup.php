83e37d6f31b5d57d46d873cbcaf3718716c89a36
project-team-ece <rahulkrishnanr2021@srishakthi.ac.in>
2021-04-11 12:38:11 +0530

    Initial commit

---
+++ backup.php
@@ -0,0 +1,28 @@
+<?php
+
+
+require_once('functions.php');
+if(!sessionCheck('level','dean'))
+  die();
+require_once('connect_db.php');
+if(valueCheck('action','backup'))
+{
+  header('Content-type: text/plain');
+  header('Content-Disposition: attachment; filename=backup_' . date("H-i_d-m-Y") . '.sql');
+  passthru("mysqldump --user={$config['db_user']} --password={$config['db_pswd']} --host={$config['db_host']} {$config['db_name']}");
+}
+else
+{
+  $snapshot = $_FILES['snapshot']['tmp_name'];
+  try
+  {
+    $db->exec(file_get_contents($snapshot));
+    unlink($snapshot);
+    header("Location: dean.php?status=restoreComplete");
+  } 
+  catch(PDOException $e)
+  {
+    postResponse("error",$e->errorInfo[2]);
+  }
+}
+?>
